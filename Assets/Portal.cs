30b474c65723572110d7488feeb77eb3daa3cc85
Anthony Smith <asmith0235@gmail.com>
2019-03-06 16:01:42 -0500

    add everything

---
+++ Assets/Portal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+    public GameObject otherPortal;
+
+    public bool portalOn = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && otherPortal.GetComponent<Portal>().portalOn)
+        {
+            Rigidbody otherRigidBody = other.GetComponent<Rigidbody>();
+
+            Vector3 exitVelocity = otherPortal.transform.forward * otherRigidBody.velocity.magnitude;
+            otherRigidBody.velocity = exitVelocity;
+
+            other.transform.position = otherPortal.transform.position + otherPortal.transform.forward * 2;
+
+        }
+    }
+    
+}
---
+++ Assets/PortalGun.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalGun : MonoBehaviour
+
+
+{
+
+    public GameObject leftPortal, rightPortal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ThrowPortal(leftPortal);
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            ThrowPortal(rightPortal);
+        }
+    }
+
+    void ThrowPortal(GameObject portal)
+    {
+        portal.GetComponent<Portal>().portalOn = true;
+
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(x, y));
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            portal.transform.position = hit.point;
+            portal.transform.rotation = Quaternion.LookRotation(hit.normal);
+        }
+    }
+}
+
